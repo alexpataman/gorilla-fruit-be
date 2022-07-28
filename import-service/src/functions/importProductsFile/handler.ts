7186438487e6b5e4b3a5bf2b99dad4b05aaabef7
Oleksandr_Pataman <oleksandr_pataman@epam.com>
2022-07-28 19:39:10 +0200

    feat: add tests for importProductsFile

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -1,11 +1,17 @@
-import { lambdaHandler } from '@/utils/lambdaHandler';
 import * as AWS from 'aws-sdk';
 
-import schema from './schema';
+import { lambdaHandler } from '@/utils/lambdaHandler';
+import { HTTP_CODE, HttpError } from '@/utils/http';
+import { BAD_REQUEST_MESSAGE } from '@/constants';
 import { BUCKET_NAME, DEFAULT_REGION, UPLOADED_DIR } from '@/constants/aws';
+
+import schema from './schema';
 
 export const main = lambdaHandler(async (event) => {
   const { name } = event.queryStringParameters;
+  if (!name) {
+    throw new HttpError(HTTP_CODE.BAD_REQUEST, BAD_REQUEST_MESSAGE);
+  }
   const s3 = new AWS.S3({ region: DEFAULT_REGION });
   const params = {
     Bucket: BUCKET_NAME,
